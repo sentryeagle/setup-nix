06706294f39a44e416c1346173d72f59c5744f23
Stefan Kühnel <git@stefankuehnel.com>
2025-05-03 20:32:34 +0200

    install-nix.sh: Add Attribution Link
    Signed-off-by: Stefan Kühnel <git@stefankuehnel.com>

--- install-nix.sh
+++ install-nix.sh
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# See: https://github.com/cachix/install-nix-action/blob/master/install-nix.sh
+
 set -euo pipefail
 
 if nix_path="$(type -p nix)" ; then
